116975df2400f3b7c69f33ddd8ab0685be3da035
Jessica Hu <jessica.hu@myob.com>
2022-05-05 21:34:32 +1000

    add tutorial for interactive map

--- src/screen/Map.tsx
+++ src/screen/Map.tsx
@@ -2,17 +2,19 @@ import * as React from 'react'
 import { StyleSheet, Dimensions } from 'react-native';
 import MapView from 'react-native-maps';
 
+// https://betterprogramming.pub/google-maps-and-places-in-a-real-world-react-native-app-100eff7474c6
+
 const Map = () => {
   return (
-      <MapView
-        initialRegion={{
-          latitude: -37.8136,
-          longitude: 144.9631,
-          latitudeDelta: 0.04,
-          longitudeDelta: 0.05,
-        }}
-        style={styles.map}
-      />
+    <MapView
+      initialRegion={{
+        latitude: -37.8136,
+        longitude: 144.9631,
+        latitudeDelta: 0.04,
+        longitudeDelta: 0.05,
+      }}
+      style={styles.map}
+    />
   )
 }
 
