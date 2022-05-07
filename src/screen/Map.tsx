272504d248b125e7ea45ce35daac5e43c55ace6c
Jessica Hu <jessica.hu@myob.com>
2022-05-07 16:15:56 +1000

    show map at current location

--- src/screen/Map.tsx
+++ src/screen/Map.tsx
@@ -1,20 +1,47 @@
 import * as React from 'react'
-import { StyleSheet, Dimensions } from 'react-native';
+import { StyleSheet, Dimensions, View, Text } from 'react-native';
 import MapView from 'react-native-maps';
-
-// https://betterprogramming.pub/google-maps-and-places-in-a-real-world-react-native-app-100eff7474c6
+import * as Location from 'expo-location';
+import { LocationObject } from 'expo-location';
+import appStyles from '../appStyles';
 
 const Map = () => {
+
+  const [location, setLocation] = React.useState<LocationObject | null>(null);
+  const [locationError, setLocationError] = React.useState("");
+
+  React.useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setLocationError('Permission to access location was denied.\nPlease grant permission and try again.');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);  
+  
   return (
-    <MapView
-      initialRegion={{
-        latitude: -37.8136,
-        longitude: 144.9631,
-        latitudeDelta: 0.04,
-        longitudeDelta: 0.05,
-      }}
-      style={styles.map}
-    />
+    <View>
+      {
+        locationError.length > 0 &&
+        <Text style={appStyles.validationError}>{locationError}</Text>
+      }
+      {
+        locationError.length === 0 && location !== null &&
+        <MapView
+          initialRegion={{
+            latitude: location?.coords.latitude,
+            longitude: location?.coords.longitude,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01,
+          }}
+          style={styles.map}
+        />
+      }
+    </View>
   )
 }
 
