198898766a9015c06e03e74ab157db7cbad06bf5
Jessica Hu <jessica.hu@myob.com>
2022-05-08 15:33:48 +1000

    colour code markers based on type

--- src/screen/Map/MapMarker.tsx
+++ src/screen/Map/MapMarker.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import { Marker } from 'react-native-maps';
-import { ParkingSpotLocation } from '../../database/parkingData';
+import { ParkingSpotLocation, ParkingSpotType } from '../../database/parkingData';
 
-const MapMarker = ({ id, name, address, latitude, longitude}: ParkingSpotLocation) => {
+const parkingColour: Record<ParkingSpotType, string> = {
+  [ParkingSpotType.Free_LotCovered]: 'purple',
+  [ParkingSpotType.Free_LotUncovered]: 'blue',
+  [ParkingSpotType.Free_Street]: 'green',
+
+  [ParkingSpotType.Paid_LotCovered]: 'yellow',
+  [ParkingSpotType.Paid_LotUncovered]: 'orange',
+  [ParkingSpotType.Paid_Street]: 'red'
+};
+
+const parkingTypeName: Record<ParkingSpotType, string> = {
+  [ParkingSpotType.Free_LotCovered]: 'Free covered parking lot',
+  [ParkingSpotType.Free_LotUncovered]: 'Free uncovered parking lot',
+  [ParkingSpotType.Free_Street]: 'Free street parking',
+
+  [ParkingSpotType.Paid_LotCovered]: 'Paid covered parking lot',
+  [ParkingSpotType.Paid_LotUncovered]: 'Paid uncovered parking lot',
+  [ParkingSpotType.Paid_Street]: 'Paid street parking'
+};
+
+const MapMarker = ({ id, name, address, type, latitude, longitude}: ParkingSpotLocation) => {
   return (
     <Marker
       key={id}
@@ -10,9 +30,9 @@ const MapMarker = ({ id, name, address, latitude, longitude}: ParkingSpotLocatio
         latitude: latitude,
         longitude: longitude
       }}
-      pinColor = {"purple"}
+      pinColor = {parkingColour[type]}
       title={name}
-      description={address}
+      description={`${address} [${parkingTypeName[type]}]`}
     />
   );
 };
