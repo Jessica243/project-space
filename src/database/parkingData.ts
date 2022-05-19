79e8cddc293a58e1bd4d64784dd839ec1d3b754b
Jessica Hu <jessica.hu@myob.com>
2022-05-20 09:47:40 +1000

    fix linting

--- src/database/parkingData.ts
+++ src/database/parkingData.ts
@@ -40,7 +40,7 @@ const parkingLocations: Array<ParkingSpotLocation> = [
     latitude: -37.819326613715134,
     longitude: 145.11990034286214,
     clearanceHeight: 2.1,
-    totalSpaces: 100
+    totalSpaces: 100,
   }, {
     id: 2,
     name: 'Boxhill Free parking',
@@ -50,7 +50,7 @@ const parkingLocations: Array<ParkingSpotLocation> = [
     latitude: -37.81854206547476,
     longitude: 145.11757658141642,
     clearanceHeight: 2.1,
-    totalSpaces: 60
+    totalSpaces: 60,
   }, {
     id: 3,
     name: 'Wilson Parking',
@@ -60,7 +60,7 @@ const parkingLocations: Array<ParkingSpotLocation> = [
     latitude: -37.81776324287638,
     longitude: 145.11703172259323,
     clearanceHeight: 2.1,
-    totalSpaces: 40
+    totalSpaces: 40,
   }, {
     id: 4,
     name: 'Wilson Parking',
@@ -70,7 +70,7 @@ const parkingLocations: Array<ParkingSpotLocation> = [
     latitude: -37.81828514447232,
     longitude: 145.11918097967268,
     clearanceHeight: 2.1,
-    totalSpaces: 150
+    totalSpaces: 150,
   }, {
     id: 5,
     name: 'Wilson Parking',
@@ -80,7 +80,7 @@ const parkingLocations: Array<ParkingSpotLocation> = [
     latitude: -37.81887011044014,
     longitude: 145.12226133457077,
     clearanceHeight: 2.1,
-    totalSpaces: 10
+    totalSpaces: 10,
   }, {
     id: 6,
     name: 'Ace Parking | 519 Station St, Box Hill',
@@ -90,7 +90,7 @@ const parkingLocations: Array<ParkingSpotLocation> = [
     latitude: -37.821130418105476,
     longitude: 145.12303684407678,
     clearanceHeight: 2.5,
-    totalSpaces: 100
+    totalSpaces: 100,
   }, {
     id: 7,
     name: 'Harrow Street Car Park',
@@ -100,7 +100,7 @@ const parkingLocations: Array<ParkingSpotLocation> = [
     latitude: -37.82127124495155,
     longitude: 145.12399063159552,
     clearanceHeight: 2.5,
-    totalSpaces: 50
+    totalSpaces: 50,
   }, {
     id: 8,
     name: 'Ellingworth Parade Harrow Street Parking Area',
@@ -110,27 +110,27 @@ const parkingLocations: Array<ParkingSpotLocation> = [
     latitude: -37.82099663235283,
     longitude: 145.1255327366492,
     clearanceHeight: 1.8,
-    totalSpaces: 30
+    totalSpaces: 30,
   }, {
     id: 9,
     name: 'Wilson Parking',
     address: '300 Lonsdale Street, Melbourne',
     price: 13,
     type: ParkingSpotType.Paid_LotUncovered,
-    latitude: -37.81158333783042, 
+    latitude: -37.81158333783042,
     longitude: 144.9635136750925,
     clearanceHeight: 2.1,
-    totalSpaces: 150
+    totalSpaces: 150,
   }, {
     id: 10,
     name: 'Wilson Parking',
     address: '300 Latrobe Street, Melbourne',
     price: 5,
     type: ParkingSpotType.Paid_LotUncovered,
-    latitude: -37.810218072937644, 
+    latitude: -37.810218072937644,
     longitude: 144.9604981994445,
     clearanceHeight: 2.1,
-    totalSpaces: 40
+    totalSpaces: 40,
   },
 
 ];
